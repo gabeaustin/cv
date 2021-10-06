36dacfabb3d699a6a8950526dc831510b6b198b9
gabeaustin <gabriel.austin2019@gmail.com>
2021-10-05 22:25:08 -0500

    added menu - working on visibility with menu

---
+++ semantic.js
@@ -0,0 +1,9 @@
+// $("header").visibility({
+//   type: "fixed",
+//   offset: 15, // give some space from top of screen
+// });
+
+$(".overlay.example .overlay").visibility({
+  type: "fixed",
+  offset: 15, // give some space from top of screen
+});
